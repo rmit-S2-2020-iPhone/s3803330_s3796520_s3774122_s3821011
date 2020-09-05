70ec089d85e4a234ed0f4db593b645d9dea75c3a
Gajendra Singh Shekhawat <gajsa.shekhawat@gmail.com>
2020-09-05 10:08:42 +1000

    cocktail list view added with controller

--- TalesAndSpirits/ViewModel/CocktailsViewController.swift
+++ TalesAndSpirits/ViewModel/CocktailsViewController.swift
@@ -8,12 +8,59 @@
 
 import UIKit
 
-class CocktailsViewController: UIViewController {
+class CocktailsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var cocktailsTableView: UITableView!
+    @IBOutlet weak var cocktailNameLabel: UILabel!
+    @IBOutlet weak var cocktailImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        cocktailsTableView.dataSource = self
+        cocktailsTableView.delegate = self
+        
         // Do any additional setup after loading the view.
     }
     
+    private var cocktails: [Cocktail] = []
+    private var favCocktails: [FavouriteCocktail] = []
+    
+    func getCocktail(byIndex index: Int) -> (cocktailName: String, image: UIImage?) {
+        
+        let cocktailName = cocktails[index].cocktailName
+        let image = UIImage(named: cocktails[index].imageName)
+        
+        return (cocktailName, image)
+        
+    }
+    
+    func getCocktail(byIndex index: Int) -> Cocktail {
+        
+        return cocktails[index]
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CKCell", for: indexPath)
+        
+        let imageView = cell.viewWithTag(1000) as? UIImageView
+        
+        let cocktailNameLabel = cell.viewWithTag(1001) as? UILabel
+        
+        if let imageView = imageView, let cocktailNameLabel = cocktailNameLabel {
+            
+//            imageView.image = UIImage(named: "liit")
+//            cocktailNameLabel.text = "Long Island Ice Tea"
+            let currentCocktail: (cocktailName: String, image: UIImage?) = CocktailsViewController.getCocktail(byIndex: indexPath.row)
+            imageView.image = currentCocktail.image
+            cocktailNameLabel.text = currentCocktail.cocktailName
+            
+        }
+        
+        return cell
+    }
     
 }
