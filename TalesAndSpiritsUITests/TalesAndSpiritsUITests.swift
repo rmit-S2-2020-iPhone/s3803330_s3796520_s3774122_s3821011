65f0797b6180ec8e32a952122d34c02871b2a6f9
Gajendra Singh Shekhawat <gajsa.shekhawat@gmail.com>
2020-10-11 03:24:58 +1100

    UI Tests updated

--- TalesAndSpiritsUITests/TalesAndSpiritsUITests.swift
+++ TalesAndSpiritsUITests/TalesAndSpiritsUITests.swift
@@ -31,8 +31,42 @@ class TalesAndSpiritsUITests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
-    func addFavourite() {
-        
+    func homeHasCollectionView()
+    {
+        let app = XCUIApplication()
+        XCTAssertTrue(app.collectionViews.element.exists)
+    }
+    
+    func favouriteIsAdded()
+    {
+        let app = XCUIApplication()
+        app.collectionViews.cells.otherElements.containing(.staticText, identifier:"Long Island Tea").element.tap()
+        app.scrollViews.otherElements.buttons["Star"].tap()
+        XCTAssertTrue(app.buttons["Star"].isSelected)
+    }
+    
+    func cocktailHasTableView()
+    {
+        let app = XCUIApplication()
+        XCUIApplication().tabBars.buttons["Cocktails"].tap()
+        XCTAssertTrue(app.tables.element.exists)
+        XCTAssertFalse(app.collectionViews.element.exists)
+    }
+    
+    func addNewCocktailViewButtonExists() {
+        let app = XCUIApplication()
+        app.tabBars.buttons["My Diary"].tap()
+        app.tables/*@START_MENU_TOKEN@*/.buttons["icons8 add 100"]/*[[".cells.buttons[\"icons8 add 100\"]",".buttons[\"icons8 add 100\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        XCTAssertTrue(app.buttons["Pick from Library"].exists)
+        XCTAssertTrue(app.textFields["CocktailName"].exists)
+    }
+    
+    func developerLabelExist()
+    {
+        let app = XCUIApplication()
+        app.tabBars.buttons["More"].tap()
+        let tf = app.staticTexts["Meet the Developers"]
+        XCTAssertTrue(tf.exists)
     }
 
 }
